544105d89f7251e17f6bc8eebe4eca295bd0df0a
feng <719542112@qq.com>
2019-02-06 20:56:05 +0100

    not finsh

--- pw03-web/src/main/java/yncrea/pw03/controller/StudentServlet.java
+++ pw03-web/src/main/java/yncrea/pw03/controller/StudentServlet.java
@@ -1,5 +1,36 @@
 package yncrea.pw03.controller;
 
-public class StudentServlet {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import yncrea.pw01.model.Drug;
+import yncrea.pw03.entity.Student;
+
+@WebServlet(urlPatterns = "/students")
+public class StudentServlet extends HttpServlet {
+	
+	private List<Student> students;
+	
+	@Override
+    public void init() throws ServletException {
+        super.init();
+        students = new ArrayList<>();
+        
+    }
+	
+	@Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("students",students);
+        req.getRequestDispatcher("Students.jsp").forward(req,resp);
+    }
+
+
+    
 }
